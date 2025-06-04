a7a53746045c50dddd34b9f6a7a18bf0e87d6f2b
Vennom-Programer <163789307+Vennom-Programer@users.noreply.github.com>
2025-06-04 11:56:25 -0300

    alterção dos nomes dos arquivos html e css e criação do arquivo js do menu

---
+++ menu/menu.js
@@ -0,0 +1,13 @@
+function animação() {
+    const menu = document.querySelector('.menu');
+    const menuItems = document.querySelectorAll('.menu-item');
+
+    menu.classList.toggle('active');
+
+    menuItems.forEach((item, index) => {
+        if (menu.classList.contains('active')) {
+            item.style.animation = `fadeIn 0.5s ease forwards ${index / 7 + 0.3}s`;
+        } else {
+            item.style.animation = '';
+        }
+    });
