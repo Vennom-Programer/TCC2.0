fde664a972e35e085904331bc934585fafd41d0b
Vennom-Programer <163789307+Vennom-Programer@users.noreply.github.com>
2025-08-27 10:32:17 -0300

    Colocação dos arquivos na pasta static

--- back-end/main.py
+++ back-end/main.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request, redirect, make_response, jsonify # pyright: ignore[reportMissingImports]
-import mysql.connector # type: ignore
+# import mysql.connector # type: ignore
 
 app = Flask(__name__)
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="cadastrarItem"
-)
+# db = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="root",
+#     database="smartreserva"
+# )
 
 @app.route('/')
 def index():
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM items")
+    # cursor = db.cursor(dictionary=True)
+    # cursor.execute("SELECT * FROM items")
+    return render_template("index.html")
