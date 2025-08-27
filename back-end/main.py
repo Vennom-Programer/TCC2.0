e625cee92e7985c4a31ca0ec2b8c6eb62a02851c
Vennom-Programer <163789307+Vennom-Programer@users.noreply.github.com>
2025-08-27 15:47:29 -0300

    Criação da route para renderizar o template de menu.html

--- back-end/main.py
+++ back-end/main.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, redirect, make_response, jsonify # pyright: ignore[reportMissingImports]
-# import mysql.connector # type: ignore
+from flask import Flask, render_template, request, redirect, make_response, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-# db = mysql.connector.connect(
+@app.route('/')
+def menu():
+    return render_template('menu.html')
+
+#mydb = mysql.connector.connect(
 #     host="localhost",
 #     user="root",
 #     password="root",
 #     database="smartreserva"
-# )
-
-@app.route('/')
-def index():
+#)
+#@app.route('/')
+#def index():
     # cursor = db.cursor(dictionary=True)
     # cursor.execute("SELECT * FROM items")
-    return render_template("index.html")
+ #return render_template("index.html")
